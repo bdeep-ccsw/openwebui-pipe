2d5430981d7c8075ff0bc3de68d17097fd6daf5b
bdeep-ccsw <69206492+bdeep-ccsw@users.noreply.github.com>
2025-03-13 10:48:59 -0400

    Update chromadb-pipeline.py

--- chromadb-pipeline.py
+++ chromadb-pipeline.py
@@ -39,12 +39,13 @@ class Pipeline:
         )
 
         # Combine results into a response
-        count = self.collection.count()
+        #count = self.collection.count()
         #retrieved_docs = [doc for doc in results["documents"][0]]
         #response_text = "Relevant Info: \n" + "\n\n".join(retrieved_docs)
         #response_text += f"[{retrieved_docs}]\n"
         #response_text += f"DOCS:[{retrieved_docs}]\n"
-        response_text = f"COUNT:[{count}]\n"
+        #response_text = f"COUNT:[{count}]\n"
         #DOCS:[{retrieved_docs}]\n"
-
+        query_engine = self.index.as_query_engine(streaming=True)
+        response_text = query_engine.query(retrieved_docs)
         return response_text
