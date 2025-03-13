877ca375098848e852312525cc347c522f1403d7
bdeep-ccsw <69206492+bdeep-ccsw@users.noreply.github.com>
2025-03-13 07:26:12 -0400

    Update chromadb-pipeline.py

--- chromadb-pipeline.py
+++ chromadb-pipeline.py
@@ -42,8 +42,8 @@ class Pipeline:
         # Combine results into a response
         retrieved_docs = [doc for doc in results["documents"][0]]
         response_text = "Relevant Info: \n" + "\n\n".join(retrieved_docs)
-        response_text += f"{self.client}\n"
-        response_text += f"{self.collection}\n"
-        response_text += f"{retrieved_docs}\n"
+        response_text += f"[{self.client}]\n"
+        response_text += f"[{self.collection}]\n"
+        response_text += f"[{retrieved_docs}]\n"
 
         return response_text
